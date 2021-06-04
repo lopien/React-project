b914f728ec0ce01d22f06cf507fe6397eeebbb9c
Kostya <romanovkostya1999@gmail.com>
2021-06-04 14:41:45 +0400

    fix modal settings

--- src/components/Header/modal.js
+++ src/components/Header/modal.js
@@ -1,15 +1,12 @@
 function ModalSettings() {
   let modalSetting = document.querySelector(".modal-setting");
   let modalBg1 = document.querySelector(".modal-bg1");
-  let body = document.querySelector("body");
 
   modalSetting.style.display = "block";
-  body.style.overflow = "hidden";
 
   modalBg1.addEventListener('click', (event) => {
     event.preventDefault();
     modalSetting.style.display = "none";
-    body.style.overflow = "auto";
   });
 }
 
