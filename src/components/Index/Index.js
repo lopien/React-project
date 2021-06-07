c03cd031c5e500e7013f4d09a2055e9fa7646aab
Kostya <romanovkostya1999@gmail.com>
2021-06-07 13:46:15 +0400

    edit modal index.js

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import OpenModal from './script.js';
+import Modal from 'react-modal';
 import "./index.css";
-
 
 const Index = () => {
   let history = useHistory();
@@ -11,30 +10,56 @@ const Index = () => {
     history.push("/main");
   }
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleRequestCloseFunc = () => {
+    setOpen(false);
+  };
+  
   return (
     <>
       <header>
         <div className="main-bg">
           <img className="main-logo" src='img/logo.png' alt="Logo"/>
-          <button onClick={OpenModal} className="btn main-registration">Регистрация</button>
-          <button onClick={OpenModal} className="btn main-login">Войти</button>
+          <button onClick={handleOpen} className="btn main-registration">Регистрация</button>
+          <button onClick={handleOpen} className="btn main-login">Войти</button>
           <div className="main-title">
             <h1 className="main-title1">Центр</h1>
             <h1 className="main-title2">Информационного</h1>
             <h1 className="main-title3">Обеспечения</h1>
           </div>
         </div>
-        <div className="modal">
-          <div className="modal-bg"></div>
-          <div className="modal-content">
+        <Modal 
+          isOpen={open}
+          onClose={handleClose}
+          className={{
+            base: 'modal-content', 
+            afterOpen: 'modal-content',
+            beforeClose: 'modal-content'
+          }}
+          overlayClassName={{
+            base: 'modal-bg', 
+            afterOpen: 'modal-bg',
+            beforeClose: 'modal-bg'
+          }}
+          onRequestClose = {handleRequestCloseFunc}
+          ariaHideApp={false}
+        >
             <h3 className="modal-title">Здравствуйте!<br/>Чтобы войти в систему введите логин и пароль</h3>
             <form className="modal-form" onSubmit={Form}>
               <input className="modal-input" type="text" placeholder="Логин" required/>
               <input className="modal-input" type="password" placeholder="Пароль" required/>
               <input type="submit" className="modal-btn" value='Вход'/>
             </form>
-          </div>
-        </div>
+        </Modal>
       </header>
     </>
   );
