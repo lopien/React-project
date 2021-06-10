c3ce9aaf35ac661b2648a1a43d97c2692df7cf4c
Kostya <romanovkostya1999@gmail.com>
2021-06-11 00:25:01 +0400

    edit news.js

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,18 +5,17 @@ import Header from '../Header/Header';
 import data from './news.json';
 
 const News = () => {
-  const template = Object.keys(data).map((item) => 
-    <div className="news" key={data[item].id}>
-    <img className="news-img" src={data[item].photo} alt=""/>
-    <p className="news-text">{data[item].text}</p>
-  </div>
-  );
-
   return(
     <>
       <Menu/>
       <Header/>
-      {template}
+      {
+        Object.keys(data).map((item) =>
+        <div className="news" key={data[item].id}>
+          <img className="news-img" src={data[item].photo} alt=""/>
+          <p className="news-text">{data[item].text}</p>
+        </div>
+      )};
     </>
     );
   };
